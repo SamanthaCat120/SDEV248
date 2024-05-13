426476e038f6e42d58e6d6dc0f383b772a1d3c6a
Samantha Lopez <53274929+SamanthaCat120@users.noreply.github.com>
2024-05-12 23:05:19 -0400

    add GodotDocTutorial

---
+++ GodotDocTutorial/HUD.gd
@@ -0,0 +1,35 @@
+extends CanvasLayer
+
+# Notifies 'Main' node that the button has been pressed
+signal start_game
+
+func show_message(text):
+	$Message.text = text
+	$Message.show()
+	$MessageTimer.start()
+	
+func show_game_over():
+	show_message("Game Over")
+	# Wait until the MessageTimer has counted down.
+	await $MessageTimer.timeout
+	
+	$Message.text = "Dodge the Creeps!"
+	$Message.show()
+	# Make a one-shot timer and wait for it to finish.
+	await get_tree().create_timer(1.0).timeout
+	$StartButton.show()
+	
+func _on_start_button_pressed():
+	$StartButton.hide()
+	start_game.emit()
+
+func _on_message_timer_timeout():
+	$Message.hide()
+
+
+func _on_start_timer_timeout():
+	pass # Replace with function body.
+
+
+func _on_score_timer_timeout():
+	pass # Replace with function body.
